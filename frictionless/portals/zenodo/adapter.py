17619efaee7ac3dd783a426eb56aa56ae5ab5853
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2024-09-16 11:27:02 +0200

    import os

--- frictionless/portals/zenodo/adapter.py
+++ frictionless/portals/zenodo/adapter.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import datetime
 import json
+import os
 import tempfile
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict, List, Union, cast
